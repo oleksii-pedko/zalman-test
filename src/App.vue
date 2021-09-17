a3910ec93e170ea5bcb4f08495b372ec50047a62
Alex Pedko <alexpedko@MacBook-Pro-Alex-2.local>
2021-09-17 12:07:19 +0300

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3" v-for="hit of hits" :key="hit">
+          <div class="card" style="width: 18rem;height: 400px!important;">
+            <img v-bind:src="hit.largeImageURL" class="card-img-top" alt="..." style="min-height: 200px">
+            <div class="card-body">
+              <h5 class="card-title">{{ hit.tags }}</h5>
+              <b class="btn btn-primary m-2">Likes: {{ hit.likes }}</b><b class="btn btn-outline-primary m-2">Commnets: {{ hit.likes }}</b>
+            </div>
+          </div>
+          <h2></h2>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      hits: []
+    }
+  },
+  mounted() {
+    axios.get('https://pixabay.com/api/?key=23430510-5f9a727293ecb6cd3e61aa9f0&q=cats&image_type=all&per_page=100')
+        .then(response => {
+          console.log(response);
+          this.hits = response.data.hits;
+        });
   }
 }
 </script>
